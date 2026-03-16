55b88e95d28d28e42db75cfc22debbc3d2def2af
Janki <janki20027@gmail.com>
2026-03-16 16:32:27 +0530

    Time: 3 ms (18.77%), Space: 47.6 MB (85.75%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        int j=0;
+        while(j<nums.length && i<nums.length){
+            if(nums[i]==0 && nums[j]!=0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+            else if(nums[i]!=0){
+                i++;
+                j++;
+            }
+            else if(nums[j]==0) j++;
+        }
+    }
+}
