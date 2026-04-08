e1695dbfe3295af7e5936b8da74225949b498002
Janki <janki20027@gmail.com>
2026-04-08 12:30:07 +0530

    Time: 317 ms (5%), Space: 47.5 MB (48.16%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -4,16 +4,13 @@ class Solution {
         for(int i=0; i<n; i++){
             int leftSum = 0;
             int rightSum = 0;
-            if(i!=0){
-                for(int j=0; j<=i-1; j++){
-                    leftSum += nums[j];
-                }
+            for(int j=0; j<i; j++){
+                leftSum += nums[j];
             }
-            if(i!=n-1){
-                for(int j=i+1; j<n; j++){
-                    rightSum += nums[j];
-                }
+            for(int j=i+1; j<n; j++){
+                rightSum += nums[j];
             }
+        
             if(leftSum==rightSum){
                 return i;
             }
