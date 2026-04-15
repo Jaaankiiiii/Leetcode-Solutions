dfa5f945b1e78566c1b2c789e389201502546ac4
Janki <janki20027@gmail.com>
2026-04-15 13:41:38 +0530

    Time: 1 ms (86.02%), Space: 45.6 MB (51.55%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,24 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        helper(nums, 0, ans);
+        ArrayList<Integer> a = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        combinations(nums, ans, a,used);
         return ans;
+        
     }
-    public void helper(int[] nums, int idx, List<List<Integer>> ans){
-        if(idx==nums.length-1){
-            List<Integer> v = new ArrayList<>();
-            for(int i=0; i<nums.length; i++){
-                v.add(nums[i]);
-            }
-            ans.add(v);
+    public void combinations(int[] nums, List<List<Integer>> ans , ArrayList<Integer> a , boolean[] used){
+        if(a.size()==nums.length){
+            ans.add(new ArrayList<>(a));
             return;
         }
-        for(int i=idx; i<nums.length; i++){
-            swap(i,idx,nums);
-            helper(nums,idx+1,ans);
-            swap(i,idx,nums);
+        for(int i=0; i<nums.length; i++){
+            if(used[i] == true) continue;
+            a.add(nums[i]);
+            used[i] = true;
+            combinations(nums, ans, a, used);
+            a.remove(a.size()-1);
+            used[i] = false;
         }
-    }
-    public void swap(int i, int idx, int[] nums){
-        int temp = nums[i];
-        nums[i] = nums[idx];
-        nums[idx] = temp;
     }
 }
