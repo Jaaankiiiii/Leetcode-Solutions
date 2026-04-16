00fa43f0d81e0c5a141918a00ae4cf7fe9748e02
Janki <janki20027@gmail.com>
2026-04-16 14:41:43 +0530

    Time: 0 ms (100%), Space: 42.4 MB (63.06%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,17 +1,11 @@
 class Solution {
     public int reverseBits(int n) {
-        int[] arr=new int[32];
-        int i=31;
-        while(n>=1){
-            arr[i--] = n%2;
-            n/=2;
+        int result = 0;
+        for(int i=0; i<32; i++){
+            result <<= 1;
+            result |= (n&1);
+            n >>= 1;
         }
-        int num = 0;
-        for(int j=0; j<arr.length; j++){
-            if(arr[j]==1){
-                num+=(1<<j);
-            }
-        }
-        return num;
+        return result;
     }
 }
