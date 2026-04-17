95692faf10c867890ca5fbafe7149cd6f24a3e4e
Janki <janki20027@gmail.com>
2026-04-18 00:22:44 +0530

    Time: 4 ms (100%), Space: 46.8 MB (17.17%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.java
@@ -0,0 +1,64 @@
+class MyCircularQueue {
+    int size;
+    int[] arr;
+    int front;
+    int rear;
+    public MyCircularQueue(int k) {
+        size = k;
+        arr = new int[k];
+        front = -1;
+        rear = -1;
+    }
+    
+    public boolean enQueue(int value) {
+        if(front==(rear+1)%size) return false;
+        if(front==-1) {
+            front=0;
+        }
+        rear = (rear+1)%size;
+        arr[rear] = value;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(front==-1) return false;
+        if(front==rear){
+            front=-1;
+            rear=-1;
+            return true;
+        }
+        else front=(front+1)%size;
+        return true;
+    }
+    
+    public int Front() {
+        if(front==-1) return -1;
+        return arr[front];
+    }
+    
+    public int Rear() {
+        if(rear==-1) return -1;
+        return arr[rear];
+    }
+    
+    public boolean isEmpty() {
+        if(front==-1) return true;
+        return false;
+    }
+    
+    public boolean isFull() {
+        if((rear + 1) % size == front) return true;
+        return false;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
