dbb612afe70e233f4734267c6de7e39a4bccd967
Janki <janki20027@gmail.com>
2026-05-24 14:39:44 +0530

    Time: 33 ms (30.24%), Space: 59.5 MB (11.79%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,39 +1,35 @@
 class Solution {
-    public class Triplet implements Comparable<Triplet>{
-        int d;
-        int x;
-        int y;
-        Triplet(int d, int x, int y){
-            this.d = d;
-            this.x = x;
-            this.y = y;
+        public class Triplet implements Comparable<Triplet>{
+            int d,x,y;
+            Triplet(int d, int x, int y){
+                this.d = d;
+                this.x = x;
+                this.y = y;
+            }
+            public int compareTo(Triplet t){
+                return  t.d-this.d;
+            }
         }
-        public int compareTo(Triplet t){
-            return this.d - t.d;
-        }
-    }
     public int[][] kClosest(int[][] points, int k) {
-        if(k==points.length) return points;
-        int n = points.length;
-        PriorityQueue<Triplet> heap = new PriorityQueue(Collections.reverseOrder());
-        for(int i=0; i<n; i++){
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        for(int i =0; i<points.length; i++){
             int x = points[i][0];
             int y = points[i][1];
-            int d = x*x + y*y;
-            heap.add(new Triplet(d,x,y));
-            if(heap.size()>k) heap.remove();
-        
+            int dist = x*x + y*y;
+            pq.add(new Triplet(dist,x,y));
+            if(pq.size()>k){
+                pq.remove();
+            }
         }
         int[][] ans = new int[k][2];
-        int index = 0;
-       for(int i=0; i<k; i++){
-            Triplet t = heap.peek();
+        int i=0;
+        while(pq.size()>0){
+            Triplet t = pq.peek();
             ans[i][0] = t.x;
             ans[i][1] = t.y;
-            //index++;
-            heap.remove();             
+            i++;
+            pq.remove();
         }
         return ans;
-
     }
 }
