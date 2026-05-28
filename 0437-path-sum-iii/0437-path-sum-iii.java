4e6b1083d5a6f71921c2c3fc39569660e0a407ef
Janki <janki20027@gmail.com>
2026-05-28 15:29:52 +0530

    Time: 20 ms (10.86%), Space: 44.9 MB (82.77%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,19 +14,19 @@
  * }
  */
 class Solution {
-    public int helper(TreeNode root, long target){
-        if(root==null) return 0;
-        int count = 0;
-        target -= root.val;
-        if(target==0) count++;
-        count += helper(root.left,target);
-        count += helper(root.right,target);
-        return count;
-    }
     public int pathSum(TreeNode root, int targetSum) {
+        if(root==null ) return 0;
+        return helper(root,targetSum) + pathSum(root.left,targetSum) + pathSum(root.right,targetSum);
+    }
+   int helper(TreeNode root, long target){
         if(root==null) return 0;
-        int count = 0;
-        return helper(root, targetSum) + pathSum(root.left,targetSum) + pathSum(root.right, targetSum);
-        
+        int count=0;
+        target -= root.val;
+        if(target==0){
+            count++;
+        }
+        count += helper(root.left,target);
+        count +=helper(root.right,target);
+        return count;
     }
 }
