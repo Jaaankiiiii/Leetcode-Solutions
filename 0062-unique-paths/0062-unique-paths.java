66301e7ab0c0933a195d808ce8f09399faa91590
Janki <janki20027@gmail.com>
2026-06-26 12:44:19 +0530

    Time: 0 ms (100%), Space: 42.1 MB (64.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[2][n];
+        int count=0;
         Arrays.fill(dp[0],1);
         Arrays.fill(dp[1],1);
         for(int i=1; i<=m-1; i++){
             for(int j=1; j<n; j++){ 
-                dp[1][j] = dp[1][j-1] + dp[0][j];
-                dp[0][j] = dp[1][j];
+                if(count%2==0){
+                    dp[1][j] = dp[1][j-1] + dp[0][j];
+                    dp[0][j] = dp[1][j];
+                }else{
+                    dp[0][j] = dp[0][j-1] + dp[1][j];
+                    dp[1][j] = dp[0][j];
+                }
             }
         }
-        return dp[1][n-1];
+        if(count%2==0) return dp[1][n-1];
+        return dp[0][n-1];
     }
 }
