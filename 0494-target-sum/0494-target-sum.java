e58704e1a865edb3802d0ba346688504fde08ab0
Janki <janki20027@gmail.com>
2026-07-02 12:27:19 +0530

    Time: 7 ms (64.89%), Space: 46.8 MB (23.3%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,24 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return totalWays(nums,0,target);
+        int n=nums.length;
+        int sum=0;
+        for(int x : nums) sum+=x;
+
+        int[][]dp = new int[n][2*sum+1];
+        for(int[] row: dp) Arrays.fill(row,-1);
+
+        return totalWays(nums,0,0,target, dp,sum);
+
     }
-    public int totalWays(int[] nums, int idx, int target){
+    public int totalWays(int[] nums, int idx,int res, int target, int[][] dp, int sum){
         //if(target==0) return 1; // will not write here cause i dont want to return the ans in between when target is found -> qn mentioned we have to use all elements ( means go till the end );
         if(idx >= nums.length){
-            if(target==0) return 1;
+            if(res==target) return 1;
             else return 0;
         }
-        int skip = totalWays(nums,idx+1,target-nums[idx]);
-        int pick = totalWays(nums,idx+1,target+nums[idx]);
-        return skip+pick;
+        if(dp[idx][res+sum]!=-1) return dp[idx][res+sum];
+        int skip = totalWays(nums,idx+1,res-nums[idx],target,dp,sum);
+        int pick = totalWays(nums,idx+1,res+nums[idx],target,dp,sum);
+        return dp[idx][res+sum]=skip+pick;
     }
 }
